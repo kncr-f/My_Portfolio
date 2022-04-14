e3e426e68a7442b4c5ff2ab6412c5a66a7a2be68
Fatih Kincir <kincirfatih@gmail.com>
2022-04-14 13:08:50 +0200

    Typing Animation added & Icons directory created

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
 import Layout from "../components/Layout";
+import React from "react";
 import { motion } from "framer-motion";
+import Typical from "react-typical";
 
+const TypingAnimation = React.memo(
+  function TypingAnimation() {
+    return (
+      <Typical
+        loop={3}
+        wrapper="p"
+        steps={[
+          "Web Developer 👑",
+          2000,
+          "React Enthusiast 💥",
+          2000,
+          "Addicted to learning  ",
+          2000,
+        ]}
+      >
 
+      </Typical>
+
+    );
+  },
+  (props, prevProps) => true
+);
 
 export default function Home() {
   return (
@@ -26,19 +49,16 @@ export default function Home() {
         <section className="h-full max-w-5xl mx-auto pt-16 md:pt-8">
           <div className="flex flex-col items-center justify-center md:items-start">
             <h4 className="text-lg text-gray-800 dark:text-white">
-              <span className="text-xl font-normal">Hello,</span> I'm
+              <span className="text-xl font-normal">Hello,</span> I am
             </h4>
             <h1 className="mt-5 text-xl font-extrabold tracking-normal text-gray-800 uppercase md:text-4xl md:mt-3 dark:text-white">Fatih Kincir</h1>
+            <div className="mt-5 text-2xl font-normal text-transparent bg-gradient-to-r from-pink-500 via-indigo-400 to-purple-700 bg-clip-text md:mt-3">
+              <TypingAnimation />
+            </div>
           </div>
         </section>
       </main>
 
     </Layout >
-
-
-
-
-
-
   )
 }
